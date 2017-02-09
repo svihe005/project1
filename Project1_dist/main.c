23f8c2fa2e1986df468892e0e8c071e4d0be6198
Dalton Stokke <stok0165@umn.edu>
2017-02-09 11:42:26 -0600

    Basic Beginning

--- Project1_dist/main.c
+++ Project1_dist/main.c
@@ -79,6 +79,28 @@ int main(int argc, char *argv[]) {
    */
 
   /*
+	we will be calling the command using --> execvp("Path",**build_argv(target.Command),(char *) NULL) .
+	
+	local variable TargetName holds the either a given target from the terminal or the first target
+	name in the make file.
+	
+	// START OF OUR CODE:
+	
+	int target_index = find_target(TargetName, targets, nTargetCount);
+	// check find_target success
+	if (target_index == -1){
+		return -2;  // should be -1, but for troubleshooting might be easier to see problem for now at -2
+	}
+	// else TargetName is in targets
+	
+	// loop through target dependencies, the body of this loop will likely use fork, wait, and maybe exec?
+	
+	for(int i = 0, i< targets[target_index].DependencyCount; i++){
+		TargetName??? or new char pointer = targets[target_index].DependencyNames[i][];
+	// might need two loops, not sure how to access the char string at index [i][0-64], is it just 0? or what...
+	}
+  
+  
    * INSERT YOUR CODE HERE
    */
 
